f845a00f008e25defaa3635c88eab8b99364fefd
JRP <jrpacheco.dev@gmail.com>
2025-04-30 16:31:17 +0100

    Add configurable port via command-line flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
@@ -10,21 +11,24 @@ import (
 
 func main() {
 
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
-	app.Logger.Println("we are running our app")
-
 	http.HandleFunc("/health", HealthCheck)
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
+	app.Logger.Printf("we are running our app %d\n", port)
 	err = server.ListenAndServe()
 
 	if err != nil {
