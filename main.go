2c7ec1c4a44b347a699b009436f44061054c4286
JRP <jrpacheco.dev@gmail.com>
2025-04-30 16:46:14 +0100

    Add Chi router and implement health check endpoint

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/unbreakablekid/femProject/internal/app"
+	"github.com/unbreakablekid/femProject/internal/routes"
 )
 
 func main() {
@@ -20,12 +21,13 @@ func main() {
 		panic(err)
 	}
 
-	http.HandleFunc("/health", HealthCheck)
+	r := routes.SetupRoutes(app)
 	server := &http.Server{
 		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
+		Handler:      r,
 	}
 
 	app.Logger.Printf("we are running our app %d\n", port)
@@ -35,8 +37,4 @@ func main() {
 		app.Logger.Fatal(err)
 	}
 
-}
-
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Status is available \n")
 }
